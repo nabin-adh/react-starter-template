6ceafae67fb04caead37e3247a8bbed644548fd6
Nabin Adhikari <nabin.adhikari@outlook.com>
2021-10-03 11:05:45 +1100

    fixed the build

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { ListGroup, ListGroupItem } from "reactstrap";
+import { ListGroup, ListGroupItem, Button } from "reactstrap";
 
 import { actionCreators } from "../store/Posts";
 import Layout from "../components/Layout";
@@ -10,12 +10,20 @@ function Posts(props) {
   const posts = useSelector((state) => state.posts);
   useEffect(() => {
     dispatch(actionCreators.requestPosts());
-  }, []);
+  }, [dispatch]);
+
+  const handleReload = (e) => {
+    e.preventDefault();
+    dispatch(actionCreators.requestPosts());
+  };
 
   if (posts.loading) return <Layout>Loading posts...</Layout>;
   return (
     <Layout>
       <h1>Post list</h1>
+      <Button onClick={handleReload} className="mb-2">
+        Reload
+      </Button>
       <ListGroup>
         {posts.posts.map((p) => (
           <ListGroupItem key={p.id}>{p.title}</ListGroupItem>
